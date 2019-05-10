2090f8c49d4e1400e51d5c0499a2df70ee9e509e
Alec Johnson <alec.johnson@hulu.com>
2019-05-09 19:02:05 -0700

    add event getters and pause/play

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,17 @@
+// get elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelector('.player__slider');
+
+//methods
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+// add event handlers
+toggle.addEventListener('click', togglePlay);

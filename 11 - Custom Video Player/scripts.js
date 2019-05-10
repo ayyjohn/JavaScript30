27f8ba5f5a92fc7b0ef07667b8f9239541a34d59
Alec Johnson <alec.johnson@hulu.com>
2019-05-09 19:24:51 -0700

    add handling for volume and playback speed updates

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,7 +5,7 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const ranges = player.querySelectorAll('.player__slider');
 
 //methods
 function togglePlay() {
@@ -22,6 +22,18 @@ function skip() {
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+function turnOnRangeMovement() {
+    this.addEventListener('mousemove', handleRangeUpdate);
+}
+
+function turnOffRangeMovement() {
+    this.removeEventListener('mousemove', handleRangeUpdate);
+}
+
 // add event handlers
 toggle.addEventListener('click', togglePlay);
 
@@ -29,4 +41,8 @@ video.addEventListener('click', togglePlay);
 video.addEventListener('play', updatePlayButton);
 video.addEventListener('pause', updatePlayButton);
 
-skipButtons.forEach(button => button.addEventListener('click', skip))
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousedown', turnOnRangeMovement));
+ranges.forEach(range => range.addEventListener('mouseup', turnOffRangeMovement));
